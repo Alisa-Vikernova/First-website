2c8d97493357f655cffd719ce06e6158c4641fd4
Alisa_Vikernova <misamanovic4@gmail.com>
2025-08-23 13:22:54 +0300

    Delete update_db.py

--- update_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("forum.db")
-cursor = conn.cursor()
-cursor.execute("ALTER TABLE messages ADD COLUMN ip TEXT;")
-conn.commit()
-conn.close()
-print("Колонка ip успешно добавлена!")
